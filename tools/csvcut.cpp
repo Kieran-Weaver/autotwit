8dfae84ab486b33f0d9c088b5b0e08e4f3b4c1d4
Kieran Weaver <weaver2@student.ubc.ca>
2021-06-04 17:32:44 -0700

    Add -n option to csvcut

--- tools/csvcut.cpp
+++ tools/csvcut.cpp
@@ -2,20 +2,23 @@
 #include <csvread.h>
 #define OPTPARSE_IMPLEMENTATION
 #include <optparse.h>
+#define PRINT_LINENUM 0xFF
 
 int main(int argc, char **argv) {
 	io::LineReader lr("STDIN", stdin);
 	const char* line = lr.next_line();
 	size_t max_col = 0;
 	std::vector<int> columns;
+	uint64_t i = 0;
 
-	// Parse options: -p prefix
 	int option;
 	struct optparse options;
 
 	optparse_init(&options, argv);
-	while ((option = optparse(&options, "0123456789")) != -1) {
-		if (option == '?') {
+	while ((option = optparse(&options, "0123456789n")) != -1) {
+		if (option == 'n') {
+			columns.emplace_back(PRINT_LINENUM);
+		} else if (option == '?') {
 			std::cerr << argv[0] << ": " << options.errmsg;
 		} else {
 			columns.emplace_back(option - '0'); // Convert to int
@@ -29,11 +32,20 @@ int main(int argc, char **argv) {
 		auto row = readRow(line);
 		if (row.size() > max_col) {
 			for (size_t i = 0; i < (columns.size() - 1); i++) {
-				std::cout << "\"" << row[columns[i]] << "\",";
+				if (columns[i] == PRINT_LINENUM) {
+					std::cout << i << ",";
+				} else {
+					std::cout << "\"" << row[columns[i]] << "\",";
+				}
 			}
-			std::cout << "\"" << row[columns.back()] << "\"\n";
+			if (columns[i] == PRINT_LINENUM) {
+				std::cout << i << "\n";
+			} else {
+				std::cout << "\"" << row[columns.back()] << "\"\n";
+			}
 		}
 		line = lr.next_line();
+		i++;
 	}
 	
 	std::cout << std::flush;
